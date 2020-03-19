41b7b841d88830eca57bd67c46a39beb4acf9e5e
yasinooo777 <58569983+yasinooo777@users.noreply.github.com>
2020-03-18 20:08:27 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,6 +1,9 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("brave new world");
+        
+        
+        
         System.out.println("change one");
     }
 }
